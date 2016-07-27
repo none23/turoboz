3f2222ccbbf31b13a1818a79b619a1eae9e3479e
none23 <none23>
2016-07-27 22:56:40 +0300

    got a ucozdb

--- _tours/ucoz_redb.py
+++ _tours/ucoz_redb.py
@@ -1,6 +1,14 @@
 import sqlite3
 import time
+import json
 import datetime
+from bs4 import BeautifulSoup
+import html5lib
+from html5lib import sanitizer, treebuilders, treewalkers, serializer
+
+VALID_TAGS_SUMMARY = ['p', 'div', ]
+VALID_TAGS_CONTENT = ['h2', 'h3', 'h4', 'p', 'ul', 'li']
+all_tags_sum = ['p', 'div']
 
 conn = sqlite3.connect('ucozdb')
 c = conn.cursor()
@@ -29,8 +37,8 @@ class dbrecord:
     def __init__(self, row):
         self.id = row[0]
         self.is_puplished = row[1]
-        self.title = row[2]
-        self.heading = row[3]
+        self.title = str(row[2])
+        self.heading = str(row[3])
         self.event_date = row[4]
         self.length = row[5]
         self.tag = row[6]
@@ -38,8 +46,8 @@ class dbrecord:
         self.valid_to = row[8]
         self.views = row[9]
         self.summary = row[10]
-        self.price = row[11]
-        self.age = row[12]
+        self.price = str(row[11])
+        self.age = str(row[12])
         self.content = row[13]
 
     def __str__(self):
@@ -72,19 +80,73 @@ def read_db_data():
 
     return dbdata
 
-def apply_to_each_in(dbdata, function):
-    for dr in dbdata:
+
+def apply_to_each_in(data, function):
+    for dr in data:
         function(dr)
 
-def cleanup_dr_field(dr, field):
-    dr_f = dr.field
-    new_dr_f =
 
-    dr.field = new_dr_f
+def cleanup_html(ugly_html, valid_tags):
+
+    soup = BeautifulSoup(ugly_html, 'lxml')
+
+    for tag in soup.findAll(True):
+        tag.attrs = {}
+        if tag.name not in all_tags_sum:
+            all_tags_sum.append(tag.name)
+        if tag.name not in valid_tags:
+            tag.hidden = True
+
+    # clean_html = soup.renderContents()
+    # clean_html = soup.prettify
+    clean_html = soup
+
+    return clean_html
+
+
+def cleanup_record(dr):
+    dr.summary = cleanup_html(dr.summary, VALID_TAGS_SUMMARY)
+    # dr.content = cleanup_html(dr.content, VALID_TAGS_CONTENT)
 
 
 dbdata = read_db_data()
-print(dbdata)
+new_dbdata = read_db_data()
+
+summaries = []
+contents = []
+
+apply_to_each_in(new_dbdata, cleanup_record)
+
+ttt = []
+
+
+def testy(new_dbdata=new_dbdata):
+    for dr in new_dbdata:
+        soup = BeautifulSoup(dr.summary, 'lxml')
+        for tag in soup.findAll(True):
+            if tag.name not in all_tags_sum:
+                tt = []
+                tt.append(tag.name)
+                tt.append(dr.id)
+                ttt.append(tt)
+                all_tags_sum.append(tag.name)
+
+
+def jsond(new_dbdata):
+    nnd = []
+    for dr in new_dbdata:
+        i = dict()
+        i['id'] = dr.id
+        i['title'] = dr.title
+        i['length'] = dr.length
+        i['tag'] = dr.tag
+        i['age'] = dr.age
+        i['summary'] = str(dr.summary)
+        # i['content'] = str(dr.content)
+        nnd.append(i)
+    with open('ucozdb.json', 'w') as thefile:
+        json.dump(nnd, thefile)
+
 
 c.close()
 conn.close()
