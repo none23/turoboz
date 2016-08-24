61c66e135dc35ce3fca16a4deb7b8bad1dcd2d60
none23 <none23>
2016-08-25 00:13:01 +0300

    page transitions

---
+++ js/_page_transitions.js
@@ -0,0 +1,65 @@
+var cache = {};
+function loadPage(url) {
+  if (cache[url]) {
+    return new Promise(function(resolve) {
+      resolve(cache[url]);
+    });
+  }
+
+  return fetch(url, {
+    method: 'GET'
+  }).then(function(response) {
+    cache[url] = response.text();
+    return cache[url];
+  });
+}
+
+var page_wrap = document.querySelector('.page_wrap');
+
+function changePage() {
+  var url = window.location.href;
+
+  loadPage(url).then(function(responseText) {
+    var wrapper = document.createElement('div');
+        wrapper.innerHTML = responseText;
+
+    var oldContent = document.querySelector('.page_content_wrap');
+    var newContent = wrapper.querySelector('.page_content_wrap');
+
+    page_wrap.appendChild(newContent);
+    animate(oldContent, newContent);
+  });
+}
+
+function animate(oldContent, newContent) {
+
+  var fadeOut = oldContent.animate({
+    opacity: [1, 0]
+  }, 200);
+
+  var fadeIn = newContent.animate({
+    opacity: [0, 1]
+  }, 200);
+
+  fadeIn.onfinish = function() {
+    oldContent.parentNode.removeChild(oldContent);
+  };
+}
+
+window.addEventListener('popstate', changePage);
+
+document.addEventListener('click', function(e) {
+  var el = e.target;
+
+  while (el && !el.href) {
+    el = el.parentNode;
+  }
+
+  if (el) {
+    e.preventDefault();
+    history.pushState(null, null, el.href);
+    changePage();
+
+    return;
+  }
+});
