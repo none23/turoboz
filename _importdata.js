8dd20659d769d373624e1bcba00908468a144aa0
none23 <none23>
2016-08-25 11:58:49 +0300

    hate oauth

--- _importdata.js
+++ _importdata.js
@@ -16,7 +16,7 @@ var compl = 0;
 //var TOKEN_DIR = (process.env.HOME || process.env.HOMEPATH ||
 //    process.env.USERPROFILE) + '/.credentials/';
 //var TOKEN_PATH = TOKEN_DIR + 'sheets.googleapis.json';
-//var TOKEN_PATH = 'sheets.googleapis.json';
+var TOKEN_PATH = 'sheets.googleapis.json';
 
 // Load client secrets from a local file.
 // {{{
