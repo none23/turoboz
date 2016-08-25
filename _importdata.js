d2b658357cd6284f24cbba9e0b88c1517832b1b3
none23 <n.anisimov.23@gmail.com>
2016-08-25 12:01:37 +0300

    hate oauth

--- _importdata.js
+++ _importdata.js
@@ -12,7 +12,7 @@ var compl = 0;
 
 // If modifying these scopes, delete your previously saved credentials
 // at ~/.credentials/sheets.googleapis.com-nodejs-quickstart.json
-//var SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly'];
+var SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly'];
 //var TOKEN_DIR = (process.env.HOME || process.env.HOMEPATH ||
 //    process.env.USERPROFILE) + '/.credentials/';
 //var TOKEN_PATH = TOKEN_DIR + 'sheets.googleapis.json';
