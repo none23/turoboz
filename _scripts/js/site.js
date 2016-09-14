b33ff0c288c90bb5bea6b0a5c73d508dc60d4160
none23 <none23>
2016-09-14 21:19:07 +0300

    fixed google analytics

--- _scripts/js/site.js
+++ _scripts/js/site.js
@@ -80,8 +80,8 @@
                 e.preventDefault();
                 changePage(etarg.href);
                 history.pushState(null, null, etarg.href);
-                // ga('set', 'page', etarg.href);
-                // ga('send', 'pageview');
+                ga('set', 'page', etarg.href);
+                ga('send', 'pageview');
                 return;
             }
         });
