1a57cec7d2a42ed75564de042d86f6f22f1b9d1e
none23 <none23>
2016-09-28 23:34:01 +0300

    v1.3.0

--- _vk-watch.js
+++ _vk-watch.js
@@ -5,14 +5,13 @@ const jsonParser = bodyParser.json()
 const port = 8888
 
 app.post('/', jsonParser, (request, response) => {
-    if (!request.body) {
-        return response.sendStatus(400)
-    } 
-    if (!request.body.type){
-        return response.sendStatus(400)
-    } else if (request.body.type === 'confirmation') {
-        return response.send('a81b533d')
-    } 
+
+    if (!request.body) { return response.sendStatus(400) }
+
+    if (!request.body.type) { return response.sendStatus(400) }
+
+    if (request.body.type === 'confirmation') { return response.send('a81b533d') }
+
     console.log(`got new request: ${request.body.object}`)
 
 
