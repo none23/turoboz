229e7d4efcdf1e794fdaaca5e70fce9e72b63932
none23 <n.anisimov.23@gmail.com>
2016-12-04 02:10:49 +0300

    refactored fetch-contentful

--- fetch-contentful.js
+++ fetch-contentful.js
@@ -7,17 +7,7 @@ const assert = require('assert')
 // }}}
 const accessToken = require('./.contentful-accessToken.json')
 const space = 'x9tc47a70skr'
-// Setup {{{
-
 const client = contentful.createClient({accessToken, space})
-/*
-const rmMd = require('remove-markdown')
-const deMd = (ulMd) => {
-  rmMd(ulMd.replace(' *', '*')).split('\n ')
-}
-*/
-
-// }}}
 // Utils {{{
 
 function stripArgDash (arg) {
@@ -31,21 +21,34 @@ function stripArgDash (arg) {
 }
 
 function saveCollection (objectToSave, collectionDir) {
-  const frontMatter = yaml.dump(objectToSave)
-  const collectionFile = `---
+  let frontMatter = yaml.dump(objectToSave)
+  let collectionFile = `---
 ${frontMatter}
 ---
 `
-  const filename = `./${collectionDir}/${objectToSave.id}.md`
+  let filename = `./${collectionDir}/${objectToSave.id}.md`
   fs.writeFile(filename, collectionFile, 'utf8', function (err) {
-    if (err) { throw err }
-    console.log(`written ${filename}`)
+    if (err) { throw err } else { console.log(`written ${filename}`) }
   })
 }
 
+function tagIndexMD (tag) {
+  return `---
+layout: tours
+title: ${tag.pageTitle}
+description: >-
+  ${tag.description}
+
+menucopy: ${tag.menuCopy}
+text: >-
+  ${tag.text}
+
+permalink: ${tag.url}
+---
+`
+}
 // }}}
 // Fetch tags {{{
-
 function fetchTags () {
   client.getEntries({
     'content_type': 'tourTag'
@@ -54,57 +57,52 @@ function fetchTags () {
       return entries
     })
     .then(entries => {
-      var tagsCatalogue = []
+      let tagsData = []
+
       for (let item of entries.items) {
         let newTag = {}
+
         newTag.tag = item.fields.id
         newTag.text = item.fields.text ? item.fields.text : ''
-        newTag.title = item.fields.pageTitle
         newTag.url = `/tours/${newTag.tag}/`
         newTag.description = item.fields.pageDescription
-        newTag.title = item.fields.pageTitle
+        newTag.pageTitle = item.fields.pageTitle
+        newTag.menuCopy = item.fields.title
+
         if (item.fields.subtags) {
           newTag.subtags = []
+
           for (let st of item.fields.subtags) {
             let newSubtag = {}
+
             newSubtag.subtag = st.fields.id
+            newSubtag.description = st.fields.pageDescription ? st.fields.pageDescription : ''
+            newSubtag.pageTitle = st.fields.pageTitle
+            newSubtag.menuCopy = st.fields.title
             newSubtag.text = st.fields.text
             newSubtag.url = `/tours/${item.fields.abbreviation}/${st.fields.abbreviation}/`
-            newSubtag.description = st.fields.pageDescription ? st.fields.pageDescription : ''
-            newSubtag.title = st.fields.pageTitle
+
             newTag.subtags.push(newSubtag)
           }
         }
-        if (!item.fields.isSubtag) { tagsCatalogue.push(newTag) }
+
+        if (!item.fields.isSubtag) { tagsData.push(newTag) }
       }
-      return tagsCatalogue
+
+      return tagsData
     })
-    .then(tagsCatalogue => {
-      for (let tag of tagsCatalogue) {
+
+    .then(tagsData => {
+      for (let tag of tagsData) {
         if (!fs.existsSync(`.${tag.url}`)) {
           fs.mkdirSync(`.${tag.url}`)
         }
+
         let tagIndex = `.${tag.url}index.html`
-        let tagIndexContent = `---
-layout: tours
-title: ${tag.title}
-description: >-
-  ${tag.description}
+        let tagIndexContent = tagIndexMD(tag)
 
-text: >-
-  ${tag.text}
-
----
-`
         fs.writeFile(tagIndex, tagIndexContent, 'utf8', function (err) {
-          if (err) {
-            throw err
-          }
-          console.log(`written
-          ---------------------
-          ${tagIndexContent}
-          ---------------------
-          to file: ${tagIndex}`)
+          if (err) { throw err } else { console.log(`written ${tagIndex}`) }
         })
 
         if (tag.subtags) {
@@ -112,58 +110,49 @@ text: >-
             if (!fs.existsSync(`.${subtag.url}`)) {
               fs.mkdirSync(`.${subtag.url}`)
             }
+
             let subtagIndex = `.${subtag.url}index.html`
-            let subtagIndexContent = `---
-layout: tours
-title: ${subtag.title}
-description: >-
-  ${subtag.description}
+            let subtagIndexContent = tagIndexMD(subtag)
 
-text: >-
-  ${subtag.text}
-
----
-`
             fs.writeFile(subtagIndex, subtagIndexContent, 'utf8', function (err) {
-              if (err) {
-                throw err
-              }
-              console.log(`written
-              ---------------------
-              ${subtagIndexContent}
-              ---------------------
-              to file: ${subtagIndex}`)
+              if (err) { throw err } else { console.log(`written ${subtagIndex}`) }
             })
           }
         }
       }
-      return tagsCatalogue
+      return tagsData
     })
-    .then(tagsCatalogue => {
+
+    .then(tagsData => {
       let filename = './_data/tags.json'
-      fs.writeFile(filename, JSON.stringify(tagsCatalogue), 'utf8', function (err) {
-        if (err) { throw err }
-        console.log(`written ${filename}`)
+      fs.writeFile(filename, JSON.stringify(tagsData), 'utf8', function (err) {
+        if (err) { throw err } console.log(`written ${filename}`)
       })
     })
+
     .catch(err => {
-      console.log(err)
+      throw err
     })
 }
 // }}}
 // Fetch tours {{{
-
 function fetchTours () {
-  var toursCatalogue = {}
-  var upcomingTours = []
-  var toursAssets = []
-  var entriesCount = 0
   client.getEntries({
     'content_type': 'tour'
   })
-    .then((entries) => {
-      for (const item of entries.items) {
-        var newTour = {}
+    .then(entries => {
+      return entries
+    })
+    .then(entries => {
+      let toursData = {}
+      let entriesCount = 0
+      let toursAssets = []
+      let upcomingTours = []
+      let response = {}
+
+      for (let item of entries.items) {
+        let newTour = {}
+
         newTour.layout = 'tour'
         newTour.id = item.fields.tour.toString()
         newTour.tour = item.fields.tour.toString()
@@ -178,7 +167,6 @@ function fetchTours () {
         newTour.tourlength = item.fields.length
         newTour.imgasset = item.fields.imgasset.fields.file.url
         newTour.imgpath = item.fields.imgasset.fields.file.fileName
-
         newTour.tourdate = item.fields.dates ? `${item.fields.dates[0]}` : '00.00.0000'
         newTour.tourlastdate = item.fields.dates && item.fields.dates[1] ? `${item.fields.dates[item.fields.dates.length - 1]}` : '00.00.0000'
         newTour.prices = item.fields.prices ? item.fields.prices.split('* ').slice(1) : ['уточняйте при заказе']
@@ -188,69 +176,93 @@ function fetchTours () {
         newTour.willLearn = item.fields.willLearn ? item.fields.willLearn.split('* ').slice(1) : []
         newTour.details = item.fields.details ? item.fields.details.split('\n\n') : []
 
+        toursData[newTour.id] = newTour
         entriesCount += 1
         saveCollection(newTour, '_tours')
         toursAssets.push([newTour.imgasset, newTour.imgpath])
-        toursCatalogue[newTour.id] = newTour
-
-        // Save the image
-        // TODO fs.writeFile(`./img/_tours/${newTour.imgpath}`, JSON.stringify(upcomings.slice(0, 4)), 'utf8', function (err) {
 
         if (newTour.tourdate !== '00.00.0000') {
-          var jsDate = Date.parse(newTour.tourdate.split('.').reverse().join('-'))
+          let jsDate = Date.parse(newTour.tourdate.split('.').reverse().join('-'))
           if (jsDate > Date.now()) {
             upcomingTours.push([newTour.id, jsDate])
           }
         }
       }
+
+      response.upcomingTours = upcomingTours
+      response.toursData = toursData
+      response.toursAssets = toursAssets
+      response.entriesCount = entriesCount
+
+      return response
     })
-    .then(() => {
-      const filename = './_data/upcoming.json'
-      const upcomingPairs = upcomingTours.sort(function (a, b) {
+
+    .then(response => {
+      let filename = './_data/tours.json'
+
+      assert.equal(response.entriesCount, Object.keys(response.toursData).length)
+
+      fs.writeFile(filename, JSON.stringify(response.toursData), 'utf8', function (err) {
+        if (err) { throw err } else { console.log(`written ${filename}`) }
+      })
+
+      return response
+    })
+
+    .then(response => {
+      let filename = './_data/upcoming.json'
+      let upcomingPairs = response.upcomingTours.sort((a, b) => {
         return a[1] - b[1]
       })
-      var upcomings = upcomingPairs.map(function (pair) {
+      let upcomings = upcomingPairs.map(pair => {
         return pair[0]
       })
+
       fs.writeFile(filename, JSON.stringify(upcomings.slice(0, 4)), 'utf8', function (err) {
-        if (err) { throw err }
-        console.log(`written ${filename}`)
+        if (err) { throw err } else { console.log(`written ${filename}`) }
       })
+
+      return response
     })
-    .then(() => {
-      const filename = './_data/tours.json'
-      assert.equal(entriesCount, Object.keys(toursCatalogue).length)
-      fs.writeFile(filename, JSON.stringify(toursCatalogue), 'utf8', function (err) {
-        if (err) { throw err }
-        console.log(`written ${filename}`)
-      })
-    })
-    .then(() => {
-      for (const tourLinksPair of toursAssets) {
-        const localFile = './img/_tours/' + tourLinksPair[1]
-        const remoteFile = 'http:' + tourLinksPair[0]
+
+    .then(response => {
+      response.toursAssets.map(tourLinksPair => {
+        let filename = './img/_tours/' + tourLinksPair[1]
+        let remoteFile = 'http:' + tourLinksPair[0]
         fetch(remoteFile)
-          .then(function (res) {
-            var dest = fs.createWriteStream(localFile)
-            res.body.pipe(dest)
+          .then(res => {
+            let localFile = fs.createWriteStream(filename)
+            res.body.pipe(localFile)
+            console.log(`written ${filename}`)
           })
-      }
+          .catch(err => {
+            throw err
+          })
+      })
+    })
+
+    .catch(err => {
+      throw err
     })
 }
 // }}}
 // Fetch news {{{
 
 function fetchNews () {
-  var newsCatalogue = {}
-  var newsAssets = []
-  var entriesCount = 0
-
   client.getEntries({
     'content_type': 'news'
   })
-    .then((entries) => {
-      for (const item of entries.items) {
-        var newNews = {}
+    .then(entries => {
+      return entries
+    })
+    .then(entries => {
+      let newsData = {}
+      let entriesCount = 0
+      let newsAssets = []
+      let response = {}
+
+      for (let item of entries.items) {
+        let newNews = {}
         newNews.layout = 'post'
         newNews.id = item.fields.id.toString()
         newNews.post = item.fields.id.toString()
@@ -262,83 +274,123 @@ function fetchNews () {
         newNews.image = item.fields.image.fields.file.fileName
         newNews.body = item.fields.content
 
+        newsData[newNews.id] = newNews
         entriesCount += 1
         saveCollection(newNews, '_news')
         newsAssets.push([newNews.imgasset, newNews.image])
-        newsCatalogue[newNews.id] = newNews
       }
+
+      response.newsData = newsData
+      response.newsAssets = newsAssets
+      response.entriesCount = entriesCount
+
+      return response
     })
-    .then(() => {
-      const filename = './_data/newsposts.json'
-      assert.equal(entriesCount, Object.keys(newsCatalogue).length)
-      fs.writeFile(filename, JSON.stringify(newsCatalogue), 'utf8', function (err) {
+    .then(response => {
+      let filename = './_data/newsposts.json'
+      assert.equal(response.entriesCount, Object.keys(response.newsData).length)
+      fs.writeFile(filename, JSON.stringify(response.newsData), 'utf8', function (err) {
         if (err) { throw err }
         console.log(`written ${filename}`)
       })
+      return response
     })
-    .then(() => {
-      for (const newsLinksPair of newsAssets) {
-        const localFile = './img/_posts/' + newsLinksPair[1]
-        const remoteFile = 'http:' + newsLinksPair[0]
+
+    .then(response => {
+      response.newsAssets.map(newsLinksPair => {
+        let filename = './img/_posts/' + newsLinksPair[1]
+        let remoteFile = 'http:' + newsLinksPair[0]
         fetch(remoteFile)
-          .then(function (res) {
-            var dest = fs.createWriteStream(localFile)
-            res.body.pipe(dest)
+          .then(res => {
+            let localFile = fs.createWriteStream(filename)
+            res.body.pipe(localFile)
+            console.log(`written ${filename}`)
           })
-      }
+          .catch(err => {
+            throw err
+          })
+      })
+    })
+
+    .catch(err => {
+      throw err
     })
 }
 // }}}
 // Fetch testim {{{
 
 function fetchTestim () {
-  var testimCatalogue = []
-  var entriesCount = 0
-
   client.getEntries({
     'content_type': 'testimonial'
   })
-    .then((entries) => {
-      for (const item of entries.items) {
-        var newTestim = {}
+    .then(entries => {
+      return entries
+    })
+
+    .then(entries => {
+      let testimData = []
+      let entriesCount = 0
+      let response = {}
+
+      for (let item of entries.items) {
+        let newTestim = {}
+
         newTestim.date = item.fields.date
         newTestim.author = item.fields.author
         newTestim.message = item.fields.message
         newTestim.dateId = item.fields.date.split('.').reverse().join('_')
 
+        testimData.push(newTestim)
         entriesCount += 1
-        testimCatalogue.push(newTestim)
       }
+
+      response.testimData = testimData
+      response.entriesCount = entriesCount
+
+      return response
     })
-    .then(() => {
-      const filename = './_data/testimonials.yml'
-      assert.equal(entriesCount, testimCatalogue.length)
-      fs.writeFile(filename, yaml.safeDump(testimCatalogue), function (err) {
-        if (err) { throw err }
-        console.log(`written ${filename}`)
+
+    .then(response => {
+      let filename = './_data/testimonials.yml'
+
+      assert.equal(response.entriesCount, response.testimData.length)
+      fs.writeFile(filename, yaml.safeDump(response.testimData), function (err) {
+        if (err) { throw err } else { console.log(`written ${filename}`) }
       })
+    })
+
+    .catch(err => {
+      throw err
     })
 }
 // }}}
 
-(() => {
+;(() => {
   const args = process.argv.map(stripArgDash).slice(2)
-  if (!process.env.DOCKER) { assert.notEqual(process.cwd().indexOf('turoboz'), -1) }
+
+  if (!process.env.DOCKER) {
+    assert.notEqual(process.cwd().indexOf('turoboz'), -1)
+  }
+
   if (args) {
-    args.forEach(function (arg) {
+    args.forEach(arg => {
       switch (arg) {
         case 'tags':
           fetchTags()
           break
+
         case 'tours':
           fetchTours()
           break
+
         case 'news':
           fetchNews()
           break
+
         case 'testim':
           fetchTestim()
           break
+
         default:
           throw new Error(`Unknown argument: '${arg}'`)
       }
